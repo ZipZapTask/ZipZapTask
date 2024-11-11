0061bc2bf740fa49046f41a7a9f0eba29978fefe
Vinay Thamara <thamarvd@mail.uc.edu>
2024-11-11 17:37:20 -0500

    Separate data into another file

---
+++ data/team_members.py
@@ -0,0 +1,50 @@
+team_members = [
+        {
+            'name': 'Sam Powers',
+            'role': 'Project manager',
+            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
+            'image_url': 'media/team/sam_powers.jpeg'
+        },
+        {
+            'name': 'Vinay Thamara',
+            'role': 'Developer',
+            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
+            'image_url': 'https://placehold.co/100x100'
+        },
+        {
+            'name': 'Sebastian Tovar',
+            'role': 'Developer',
+            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
+            'image_url': 'media/team/sebastian_tovar.jpeg'
+        },
+        {
+            'name': 'Alex Jones',
+            'role': 'Developer',
+            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
+            'image_url': 'media/team/alex_jones.jpeg'
+        },
+        {
+            'name': 'Jaecar Ly',
+            'role': 'Designer',
+            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
+            'image_url': 'media/team/jaecar_ly.jpeg'
+        },
+        {
+            'name': 'Eric Atkins',
+            'role': 'Research & Design',
+            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
+            'image_url': 'media/team/eric_atkins.jpeg'
+        },
+        {
+            'name': 'Zach Cobb',
+            'role': 'Research',
+            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
+            'image_url': 'https://placehold.co/100x100'
+        },
+        {
+            'name': 'Nick Blank',
+            'role': 'Research',
+            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
+            'image_url': 'media/team/nick_blank.jpeg'
+        }
+    ]
