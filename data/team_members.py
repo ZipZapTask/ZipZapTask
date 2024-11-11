89565cf70965b85859dd745548323c8948e3199a
Vinay Thamara <thamarvd@mail.uc.edu>
2024-11-11 17:48:28 -0500

    Add users description

--- data/team_members.py
+++ data/team_members.py
@@ -1,50 +1,58 @@
 team_members = [
-        {
-            'name': 'Sam Powers',
-            'role': 'Project manager',
-            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
-            'image_url': 'media/team/sam_powers.jpeg'
-        },
-        {
-            'name': 'Vinay Thamara',
-            'role': 'Developer',
-            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
-            'image_url': 'https://placehold.co/100x100'
-        },
-        {
-            'name': 'Sebastian Tovar',
-            'role': 'Developer',
-            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
-            'image_url': 'media/team/sebastian_tovar.jpeg'
-        },
-        {
-            'name': 'Alex Jones',
-            'role': 'Developer',
-            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
-            'image_url': 'media/team/alex_jones.jpeg'
-        },
-        {
-            'name': 'Jaecar Ly',
-            'role': 'Designer',
-            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
-            'image_url': 'media/team/jaecar_ly.jpeg'
-        },
-        {
-            'name': 'Eric Atkins',
-            'role': 'Research & Design',
-            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
-            'image_url': 'media/team/eric_atkins.jpeg'
-        },
-        {
-            'name': 'Zach Cobb',
-            'role': 'Research',
-            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
-            'image_url': 'https://placehold.co/100x100'
-        },
-        {
-            'name': 'Nick Blank',
-            'role': 'Research',
-            'description': 'This is some sample content for the card. You can put any text or additional elements here.',
-            'image_url': 'media/team/nick_blank.jpeg'
-        }
-    ]
+    {
+        'name': 'Sam Powers',
+        'role': 'Project manager',
+        'description': 'As project manager it is my job to make sure that the requirement and deadlines are met as well'
+                       ' as helping everyone on the team has the things they need to Compleat there tasks',
+        'image_url': 'media/team/sam_powers.jpeg'
+    },
+    {
+        'name': 'Vinay Thamara',
+        'role': 'Developer',
+        'description': 'As a software developer, it is my job to make web pages and make sure things work as intended.',
+        # 'image_url': 'https://placehold.co/100x100'
+    },
+    {
+        'name': 'Sebastian Tovar',
+        'role': 'Developer',
+        # 'description': 'This is some sample content for the card. You can put any text or additional elements here.',
+        'image_url': 'media/team/sebastian_tovar.jpeg'
+    },
+    {
+        'name': 'Alex Jones',
+        'role': 'Developer',
+        # 'description': 'This is some sample content for the card. You can put any text or additional elements here.',
+        'image_url': 'media/team/alex_jones.jpeg'
+    },
+    {
+        'name': 'Jaecar Ly',
+        'role': 'Designer',
+        'description': 'As a designer, I worked on creating the initial layout of pages and created the Gantt chart.',
+        'image_url': 'media/team/jaecar_ly.jpeg'
+    },
+    {
+        'name': 'Eric Atkins',
+        'role': 'Research & Design',
+        'description': 'As a designer, my job is to create/design/draft visual elements of the project, including but '
+                       'not limited to page mockups, prototypes, visual designs, and formatting',
+        'image_url': 'media/team/eric_atkins.jpeg'
+    },
+    {
+        'name': 'Zach Cobb',
+        'role': 'Research',
+        'description': 'As a researcher, my job is to research what goes into making a functional and successful '
+                       'website app and assist in designing when needed.',
+        # 'image_url': 'https://placehold.co/100x100'
+    },
+    {
+        'name': 'Nick Blank',
+        'role': 'Research',
+        'description': 'My job is to create a website that includes figuring out what the website is for and who will '
+                       'use it. They gather and check all the information that will go on the site. They also talk to '
+                       'people and test the site to make sure it’s easy to use. Researchers look at other similar '
+                       'websites to see what works and what doesn’t. They find the best words to help the site show up '
+                       'in search engines. Finally, they use tools to see how well the site is doing and what visitors '
+                       'are doing on it, then use this information to make improvements.',
+        'image_url': 'media/team/nick_blank.jpeg'
+    }
+]
